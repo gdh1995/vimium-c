b9ef9d2bb10466b33c23356f05712f8acd397c47
gdh1995 <gdh1995@qq.com>
2014-12-20 20:54:50 +0800

    merge: better upgradeNotification

--- content_scripts/vimium_frontend.js
+++ content_scripts/vimium_frontend.js
@@ -1216,7 +1216,7 @@
     },
     showUpgradeNotification: function(version) {
       var el = HUD.upgradeNotificationElement(), links;
-      el.innerHTML = "Vimium has been updated to <a class='vimB vimI vimL' target='_blank' href='https://chrome.google.com/extensions/detail/dbepggeogbaibhgnhhndojpepiihcmeb'> " + version + "</a>.<a class='vimB vimI vimL vimiumHUDClose' href='#'>&#215;</a>";
+      el.innerHTML = "Vimium has been updated to " + version + " (<a class='vimB vimI vimL' target='_blank' href='https://github.com/philc/vimium#release-notes'>what's new</a>).<a class='vimB vimI vimL vimiumHUDClose' href='#'>&#215;</a>";
       links = el.getElementsByTagName("a");
       links[0].addEventListener("click", HUD.onUpdateLinkClicked, false);
       links[1].addEventListener("click", function(event) {
