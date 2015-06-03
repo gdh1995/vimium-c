82b8dd868a41ed86be82802d0fdcbc9ef814de55
gdh1995 <gdh1995@qq.com>
2015-06-03 11:21:28 +0800

    Revert 3ca924bcded7b3e82902843b85a132ff1dae66c7

--- content/inject-end.js
+++ content/inject-end.js
@@ -3,10 +3,12 @@ Settings.RequestHandlers.regExt = function(request) {
   if (this.reg(request)) {
     return true;
   }
+  if (chrome.runtime.id) {
     MainPort.postMessage({
       handler: "regExt",
       extId: chrome.runtime.id
     });
+  }
 };
 
 DomUtils.DocumentReady(function() {
