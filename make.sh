a25be9199f2e55c3227e4384bbdf7027053f62a1
gdh1995 <gdh1995@qq.com>
2015-06-21 02:05:42 +0800

    add scripts to auto build .zip file

---
+++ make.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+if [ -z "$1" ]; then
+OUTPUT=/wo/vimium++_1.49.crx
+else
+OUTPUT="$1"
+fi
+shift
+
+if [ "$OUTPUT" != "-" -a -f "$OUTPUT" ]; then
+ARGS="-u $*"
+else
+ARGS="$*"
+fi
+
+zip -r0oX -MM $ARGS "$OUTPUT" . -x ".*" "*.sh" "weidu/*" "test/*"
