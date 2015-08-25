372587f1b019a9f5b74e6521bb2b738cb05abc7e
gdh1995 <gdh1995@qq.com>
2015-08-26 01:15:42 +0800

    rework FrameMask.focus so that there will be no crash in XML pages

--- content/frontend.js
+++ content/frontend.js
@@ -1049,13 +1049,16 @@ var Settings, VHUD, MainPort, VInsertMode;
       var _this = FrameMask, dom1;
       if (dom1 = _this.node) {
         _this.more = true;
-      } else {
-        dom1 = _this.node = document.createElement("div");
+      } else if ((dom1 = document.createElement("div")).style) {
         dom1.className = "R";
         dom1.id = "HighlightMask";
-        DomUtils.UI.addElement(dom1);
+        DomUtils.UI.addElement(_this.node = dom1);
         _this.more = false;
         _this.timer = setInterval(_this.Remove, _this.timeout);
+      } else {
+        FrameMask = null;
+        requestHandlers.focusFrame = function() {};
+        return;
       }
       dom1.style.borderColor = request.frameId !== -1 ? "yellow" : "lightsalmon";
     },
