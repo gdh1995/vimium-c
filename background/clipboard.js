3be8cec5cd302ed5706a94cecd655e6ce27d2835
gdh1995 <gdh1995@qq.com>
2015-09-19 01:01:32 +0800

    delay: Clipboard

--- background/clipboard.js
+++ background/clipboard.js
@@ -1,17 +1,17 @@
 "use strict";
 var Clipboard = {
-  _textArea: null,
-  _getTextArea: function() {
-    var el = this._textArea;
+  textArea: null,
+  getTextArea: function() {
+    var el = this.textArea;
     if (! el) {
-      el = this._textArea = document.createElement("textarea");
+      el = this.textArea = document.createElement("textarea");
       el.style.position = "absolute";
       el.style.left = "-100%";
     }
     return el;
   },
   copy: function(data) {
-    var textArea = this._getTextArea();
+    var textArea = this.getTextArea();
     textArea.value = data;
     document.documentElement.appendChild(textArea);
     textArea.select();
@@ -20,7 +20,7 @@ var Clipboard = {
     textArea.value = "";
   },
   paste: function() {
-    var textArea = this._getTextArea(), value;
+    var textArea = this.getTextArea(), value;
     document.documentElement.appendChild(textArea);
     textArea.focus();
     document.execCommand("Paste");
