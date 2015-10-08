4ea628e75dc4c5b15d2dbd1e8e1cd3e7f3bf7514
gdh1995 <gdh1995@qq.com>
2015-10-09 03:16:24 +0800

    clean code about ExclusionRulesOption in options.js

--- pages/options.js
+++ pages/options.js
@@ -56,171 +56,160 @@ Option.prototype.areEqual = function(a, b) {
   return a === b;
 };
 
-ExclusionRulesOption = (function(_super) {
-  __extends(ExclusionRulesOption, _super);
+function ExclusionRulesOption() {
+  this.template = $('exclusionRuleTemplate').content.children[0];
+  ExclusionRulesOption.__super__.constructor.apply(this, arguments);
+  $("exclusionAddButton").addEventListener("click", this.addRule.bind(this, null));
+  this.element.addEventListener("input", this.onUpdated);
+  this.element.addEventListener("click", this.onRemoveRow.bind(this));
+}
+__extends(ExclusionRulesOption, Option);
 
-  function ExclusionRulesOption() {
-    this.template = $('exclusionRuleTemplate').content.children[0];
-    ExclusionRulesOption.__super__.constructor.apply(this, arguments);
-    $("exclusionAddButton").addEventListener("click", this.addRule.bind(this, null));
-    this.element.addEventListener("input", this.onUpdated);
-    this.element.addEventListener("click", this.onRemoveRow.bind(this));
-  }
-
-  ExclusionRulesOption.prototype.addRule = function(pattern) {
-    var element, exclusionScrollBox;
-    element = this.appendRule(this.element, {
-      pattern: pattern || "",
-      passKeys: ""
-    });
-    this.getPattern(element).focus();
-    exclusionScrollBox = this.element.parentElement;
-    exclusionScrollBox.scrollTop = exclusionScrollBox.scrollHeight;
-    if (pattern) {
-      this.onUpdated();
-    }
-    return element;
-  };
-
-  ExclusionRulesOption.prototype.populateElement = function(rules) {
-    var frag = document.createDocumentFragment(), head;
-    head = this.element.querySelector('tr');
-    frag.appendChild(head);
-    this.element.innerHTML = "";
-    rules.forEach(this.appendRule.bind(this, frag));
-    this.element.appendChild(frag);
-  };
-
-  ExclusionRulesOption.prototype.appendRule = function(list, rule) {
-    var row;
-    row = document.importNode(this.template, true);
-    row.querySelector('.pattern').value = rule.pattern;
-    row.querySelector('.passKeys').value = rule.passKeys;
-    list.appendChild(row);
-    return row;
-  };
-  
-  ExclusionRulesOption.prototype.onRemoveRow = function(event) {
-    if (!event.target.classList.contains("exclusionRemoveButton")) {
-      return;
-    }
-    var row1 = event.target.parentNode.parentNode;
-    if (! row1.classList.contains("exclusionRuleInstance")) {
-      return;
-    }
-    row1.parentNode.removeChild(row1);
+ExclusionRulesOption.prototype.addRule = function(pattern) {
+  var element, exclusionScrollBox;
+  element = this.appendRule(this.element, {
+    pattern: pattern || "",
+    passKeys: ""
+  });
+  this.getPattern(element).focus();
+  exclusionScrollBox = this.element.parentElement;
+  exclusionScrollBox.scrollTop = exclusionScrollBox.scrollHeight;
+  if (pattern) {
     this.onUpdated();
-  };
+  }
+  return element;
+};
 
-  ExclusionRulesOption.prototype.readValueFromElement = function(part) {
-    var element, passKeys, pattern, rules, _i, _len, _ref, wchRe;
-    rules = [];
-    wchRe = /\s+/;
-    _ref = this.element.getElementsByClassName("exclusionRuleInstance");
-    part = (part === true);
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      element = _ref[_i];
-      if (part && element.style.display === "none") {
-        continue;
-      }
-      pattern = this.getPattern(element).value.trim();
-      if (!pattern) {
-        continue;
-      }
-      passKeys = this.getPassKeys(element).value.replace(wchRe, " ").trim();
-      rules.push({
-        pattern: pattern,
-        passKeys: passKeys
-      });
+ExclusionRulesOption.prototype.populateElement = function(rules) {
+  var frag = document.createDocumentFragment(), head;
+  head = this.element.querySelector('tr');
+  frag.appendChild(head);
+  this.element.innerHTML = "";
+  rules.forEach(this.appendRule.bind(this, frag));
+  this.element.appendChild(frag);
+};
+
+ExclusionRulesOption.prototype.appendRule = function(list, rule) {
+  var row;
+  row = document.importNode(this.template, true);
+  row.querySelector('.pattern').value = rule.pattern;
+  row.querySelector('.passKeys').value = rule.passKeys;
+  list.appendChild(row);
+  return row;
+};
+
+ExclusionRulesOption.prototype.onRemoveRow = function(event) {
+  if (!event.target.classList.contains("exclusionRemoveButton")) {
+    return;
+  }
+  var row1 = event.target.parentNode.parentNode;
+  if (! row1.classList.contains("exclusionRuleInstance")) {
+    return;
+  }
+  row1.parentNode.removeChild(row1);
+  this.onUpdated();
+};
+
+ExclusionRulesOption.prototype.readValueFromElement = function(part) {
+  var element, passKeys, pattern, rules, _i, _len, _ref, wchRe;
+  rules = [];
+  wchRe = /\s+/;
+  _ref = this.element.getElementsByClassName("exclusionRuleInstance");
+  part = (part === true);
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    element = _ref[_i];
+    if (part && element.style.display === "none") {
+      continue;
     }
-    if (JSON.stringify(rules) === JSON.stringify(bgSettings.defaults[this.field])) {
-      rules = bgSettings.defaults[this.field];
+    pattern = this.getPattern(element).value.trim();
+    if (!pattern) {
+      continue;
     }
-    return rules;
-  };
+    passKeys = this.getPassKeys(element).value.replace(wchRe, " ").trim();
+    rules.push({
+      pattern: pattern,
+      passKeys: passKeys
+    });
+  }
+  if (JSON.stringify(rules) === JSON.stringify(bgSettings.defaults[this.field])) {
+    rules = bgSettings.defaults[this.field];
+  }
+  return rules;
+};
 
-  ExclusionRulesOption.prototype.flatten = function(rule) {
-    return (rule && rule.pattern) ? (rule.pattern + "\r" + rule.passKeys) : "";
-  };
+ExclusionRulesOption.prototype.flatten = function(rule) {
+  return (rule && rule.pattern) ? (rule.pattern + "\r" + rule.passKeys) : "";
+};
 
-  ExclusionRulesOption.prototype.areEqual = function(a, b) {
-    return a.map(this.flatten).join("\n") === b.map(this.flatten).join("\n");
-  };
+ExclusionRulesOption.prototype.areEqual = function(a, b) {
+  return a.map(this.flatten).join("\n") === b.map(this.flatten).join("\n");
+};
 
-  ExclusionRulesOption.prototype.getPattern = function(element) {
-    return element.querySelector(".pattern");
-  };
+ExclusionRulesOption.prototype.getPattern = function(element) {
+  return element.querySelector(".pattern");
+};
 
-  ExclusionRulesOption.prototype.getPassKeys = function(element) {
-    return element.querySelector(".passKeys");
-  };
-
-  return ExclusionRulesOption;
-
-})(Option);
+ExclusionRulesOption.prototype.getPassKeys = function(element) {
+  return element.querySelector(".passKeys");
+};
 
 if (isPopup)
-ExclusionRulesOnPopupOption = (function(_super) {
-  __extends(ExclusionRulesOnPopupOption, _super);
+function ExclusionRulesOnPopupOption(url) {
+  this.url = url;
+  ExclusionRulesOnPopupOption.__super__.constructor.apply(this,
+    2 <= arguments.length ? Array.prototype.slice.call(arguments, 1) : []);
+  this.element.addEventListener("input", this.onInput.bind(this));
+}
+__extends(ExclusionRulesOnPopupOption, ExclusionRulesOption);
 
-  function ExclusionRulesOnPopupOption(url) {
-    this.url = url;
-    ExclusionRulesOnPopupOption.__super__.constructor.apply(this,
-      2 <= arguments.length ? Array.prototype.slice.call(arguments, 1) : []);
-    this.element.addEventListener("input", this.onInput.bind(this));
+ExclusionRulesOnPopupOption.prototype.addRule = function() {
+  ExclusionRulesOnPopupOption.__super__.addRule.call(this, this.generateDefaultPattern());
+};
+
+ExclusionRulesOnPopupOption.prototype.populateElement = function(rules) {
+  var element, elements, haveMatch, pattern, _i, _len;
+  ExclusionRulesOnPopupOption.__super__.populateElement.call(this, rules);
+  elements = this.element.getElementsByClassName("exclusionRuleInstance");
+  haveMatch = -1;
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    element = elements[_i];
+    pattern = this.getPattern(element).value.trim();
+    if (bgExclusions.re[pattern](this.url)) {
+      haveMatch = _i;
+    } else {
+      element.style.display = "none";
+    }
   }
+  if (haveMatch >= 0) {
+    this.getPassKeys(elements[haveMatch]).focus();
+  } else {
+    this.addRule();
+  }
+};
 
-  ExclusionRulesOnPopupOption.prototype.addRule = function() {
-    ExclusionRulesOnPopupOption.__super__.addRule.call(this, this.generateDefaultPattern());
-  };
+ExclusionRulesOnPopupOption.prototype.onInput = function(event) {
+  var patternElement = event.target;
+  if (!patternElement.classList.contains("pattern")) {
+    return;
+  }
+  if (bgExclusions.getRe(patternElement.value)(this.url)) {
+    patternElement.title = patternElement.style.color = "";
+  } else {
+    patternElement.style.color = "red";
+    patternElement.title = "Red text means that the pattern does not\nmatch the current URL.";
+  }
+};
 
-  ExclusionRulesOnPopupOption.prototype.populateElement = function(rules) {
-    var element, elements, haveMatch, pattern, _i, _len;
-    ExclusionRulesOnPopupOption.__super__.populateElement.call(this, rules);
-    elements = this.element.getElementsByClassName("exclusionRuleInstance");
-    haveMatch = -1;
-    for (_i = 0, _len = elements.length; _i < _len; _i++) {
-      element = elements[_i];
-      pattern = this.getPattern(element).value.trim();
-      if (bgExclusions.re[pattern](this.url)) {
-        haveMatch = _i;
-      } else {
-        element.style.display = "none";
-      }
-    }
-    if (haveMatch >= 0) {
-      this.getPassKeys(elements[haveMatch]).focus();
-    } else {
-      this.addRule();
-    }
-  };
-  
-  ExclusionRulesOnPopupOption.prototype.onInput = function(event) {
-    var patternElement = event.target;
-    if (!patternElement.classList.contains("pattern")) {
-      return;
-    }
-    if (bgExclusions.getRe(patternElement.value)(this.url)) {
-      patternElement.title = patternElement.style.color = "";
-    } else {
-      patternElement.style.color = "red";
-      patternElement.title = "Red text means that the pattern does not\nmatch the current URL.";
-    }
-  };
+ExclusionRulesOnPopupOption.prototype.httpRe = /^https?:\/\/./;
+ExclusionRulesOnPopupOption.prototype.urlRe = /^[a-z]{3,}:\/\/./;
+ExclusionRulesOnPopupOption.prototype.generateDefaultPattern = function() {
+  return this.httpRe.test(this.url)
+    ? ("https?://" + this.url.split("/", 3)[2] + "/")
+    : this.urlRe.test(this.url)
+    ? (this.url.split("/", 3).join("/") + "/")
+    : this.url;
+};
 
-  ExclusionRulesOnPopupOption.prototype.httpRe = /^https?:\/\/./;
-  ExclusionRulesOnPopupOption.prototype.urlRe = /^[a-z]{3,}:\/\/./;
-  ExclusionRulesOnPopupOption.prototype.generateDefaultPattern = function() {
-    return this.httpRe.test(this.url)
-      ? ("https?://" + this.url.split("/", 3)[2] + "/")
-      : this.urlRe.test(this.url)
-      ? (this.url.split("/", 3).join("/") + "/")
-      : this.url;
-  };
-
-  return ExclusionRulesOnPopupOption;
-
-})(ExclusionRulesOption);
 
 if (isPopup)
 chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
