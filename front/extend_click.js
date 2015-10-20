9a7418bea2fcdc7ef3f692121b689e0abc560c66
gdh1995 <gdh1995@qq.com>
2015-10-20 16:50:39 +0800

    extend_click: rework injected function to refer a credible setTimeout
    just in case websites hide native setTimeout, Array::map or Function::bind
    Example: jsfiddle's top frame hides Function::bind

--- front/extend_click.js
+++ front/extend_click.js
@@ -1,7 +1,7 @@
 "use strict";
 (function(func) {
   var script, parent, installer, onclick, container;
-  window.addEventListener("VimiumRegistrationElementEvent", installer = function (event) {
+  window.addEventListener("VimiumRegistrationElementEvent", installer = function(event) {
     window.removeEventListener("VimiumRegistrationElementEvent", installer, true);
     container = event.target;
     container.addEventListener("VimiumRegistrationElementEvent-click", onclick, true);
@@ -25,45 +25,42 @@
   setTimeout(function() { script.remove(); }, 16);
 })(function() {
 "use strict";
-var _listen, toRegister, handler, register, container;
+var _listen, container, handler, reg, register, toRegister;
 
-_listen = Element.prototype.addEventListener;
+_listen = EventTarget.prototype.addEventListener;
 toRegister = [];
-container = null;
-
-handler = function(type) {
+register = function(element) { toRegister.push(element); };
+EventTarget.prototype.addEventListener = function(type) {
   if (type === "click" && this instanceof Element) {
-    setTimeout(register, 0, this);
+    register(this);
   }
   return _listen.apply(this, arguments);
 };
-EventTarget.prototype.addEventListener = handler;
 
 handler = function() {
   container = document.createElementNS("http://www.w3.org/1999/xhtml", "div");
   document.documentElement.appendChild(container);
   container.dispatchEvent(new CustomEvent("VimiumRegistrationElementEvent"));
   document.documentElement.removeChild(container);
-  register = function(element) {
-    var e1, e2, registrationEvent, wrapIncontainer;
-    wrapIncontainer = !document.contains(element);
-    if (wrapIncontainer) {
-      for (e1 = element; (e2 = e1.parentElement) != null; e1 = e2) {}
-      if (e1.parentNode instanceof ShadowRoot) { return; }
-      container.appendChild(e1);
-    }
-    registrationEvent = new CustomEvent("VimiumRegistrationElementEvent-onclick");
-    element.dispatchEvent(registrationEvent);
-    if (wrapIncontainer) {
-      container.removeChild(e1);
-    }
-  };
-  toRegister.map(function(element) { setTimeout(register, 0, element); });
+  register = reg;
+  for (var i = toRegister.length; 0 <= --i; ) { register(toRegister[i]); }
   window.removeEventListener("DOMContentLoaded", handler, true);
-  handler = toRegister = null;
+  handler = toRegister = reg = null;
 };
 _listen.call(window, "DOMContentLoaded", handler, true);
-register = function(element) {
-  toRegister.push(element);
-};
+
+reg = setTimeout.bind(window, function(element) {
+  var e1, e2, registrationEvent, wrapIncontainer;
+  wrapIncontainer = !document.contains(element);
+  if (wrapIncontainer) {
+    for (e1 = element; (e2 = e1.parentElement) != null; e1 = e2) {}
+    if (e1.parentNode instanceof ShadowRoot) { return; }
+    container.appendChild(e1);
+  }
+  registrationEvent = new CustomEvent("VimiumRegistrationElementEvent-onclick");
+  element.dispatchEvent(registrationEvent);
+  if (wrapIncontainer) {
+    container.removeChild(e1);
+  }
+}, 0);
 });
