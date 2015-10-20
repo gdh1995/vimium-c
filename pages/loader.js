65fe0466c86ffe871d96d5f0c64e9b592281a48f
gdh1995 <gdh1995@qq.com>
2015-10-20 12:54:08 +0800

    rework pages/loader: ignore extend_click

--- pages/loader.js
+++ pages/loader.js
@@ -1,6 +1,6 @@
 "use strict";
 (function() {
-  var insertLocation, insertLink, insertJS;
+  var insertLocation, insertJS, arr;
   insertLocation = document.querySelector('script[src$="content_script_loader.js"]') || document.head.firstChild;
   insertJS = function(src) {
     var scriptElement = document.createElement("script");
@@ -9,5 +9,7 @@
     scriptElement.src = (src[0] !== "/") ? ("/" + src) : src;
     insertLocation.parentElement.insertBefore(scriptElement, insertLocation);
   };
-  chrome.runtime.getManifest().content_scripts[0].js.forEach(insertJS);
+  arr = chrome.runtime.getManifest().content_scripts[0].js;
+  arr.pop();
+  arr.forEach(insertJS);
 })();
