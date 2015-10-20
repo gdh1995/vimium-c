e827f74f8caeafda61f8d33d617da317b56bb416
gdh1995 <gdh1995@qq.com>
2015-10-20 12:54:56 +0800

    noop; better pages/loader

--- pages/loader.js
+++ pages/loader.js
@@ -1,15 +1,14 @@
 "use strict";
 (function() {
-  var insertLocation, insertJS, arr;
+  var insertLocation, arr;
   insertLocation = document.querySelector('script[src$="content_script_loader.js"]') || document.head.firstChild;
-  insertJS = function(src) {
+  arr = chrome.runtime.getManifest().content_scripts[0].js;
+  arr.pop();
+  arr.forEach(function(src) {
     var scriptElement = document.createElement("script");
     scriptElement.type = "text/javascript";
     scriptElement.async = false;
     scriptElement.src = (src[0] !== "/") ? ("/" + src) : src;
     insertLocation.parentElement.insertBefore(scriptElement, insertLocation);
-  };
-  arr = chrome.runtime.getManifest().content_scripts[0].js;
-  arr.pop();
-  arr.forEach(insertJS);
+  });
 })();
