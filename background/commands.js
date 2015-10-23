4d607114160b53875648b085872924dd5a4d35bf
gdh1995 <gdh1995@qq.com>
2015-10-23 14:25:13 +0800

    rework the format of commands' declaration

--- background/commands.js
+++ background/commands.js
@@ -209,7 +209,7 @@ Commands.defaultKeyMappings = {
 };
 
 setTimeout(function(descriptions) {
-  var command, description, ref, options;
+  var command, description, ref;
   if (Commands.initIsSlow) {
     setTimeout(function() {
       Commands.parseKeyMappings(Settings.get("keyMappings"), 1);
@@ -222,473 +222,110 @@ setTimeout(function(descriptions) {
       console.warn("Bug:", command, "is already defined!");
       continue;
     } // #endif
-    options = description[1];
     ref[command] = {
-      background: options && options.background ? true : false,
+      background: description[2] || false,
       description: description[0],
-      repeat: options && options.repeat || 0
+      repeat: description[1] || 0
     };
   }
 }, 67, {
-  showHelp: [
-    "Show help", {
-      repeat: 1
-    }
-  ],
-  debugBackground: [
-    "Debug the background page", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  blank: [
-    "Do nothing", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  toggleLinkHintCharacters: [
-    "Toggle the other link hints (use value)", {
-      repeat: 1
-    }
-  ],
-  toggleSwitchTemp: [
-    "Toggle switch only in currnet page (use key[, value])", {
-      repeat: 1
-    }
-  ],
-  scrollDown: ["Scroll down"],
-  scrollUp: ["Scroll up"],
-  scrollLeft: ["Scroll left"],
-  scrollRight: ["Scroll right"],
-  scrollPxDown: ["Scroll 1px down"],
-  scrollPxUp: ["Scroll 1px up"],
-  scrollPxLeft: ["Scroll 1px left"],
-  scrollPxRight: ["Scroll 1px right"],
-  scrollToTop: [
-    "Scroll to the top of the page", {
-      repeat: 1
-    }
-  ],
-  scrollToBottom: [
-    "Scroll to the bottom of the page", {
-      repeat: 1
-    }
-  ],
-  scrollToLeft: [
-    "Scroll all the way to the left", {
-      repeat: 1
-    }
-  ],
-  scrollToRight: [
-    "Scroll all the way to the right", {
-      repeat: 1
-    }
-  ],
-  scrollPageDown: ["Scroll a page down"],
-  scrollPageUp: ["Scroll a page up"],
-  scrollFullPageDown: ["Scroll a full page down"],
-  scrollFullPageUp: ["Scroll a full page up"],
-  reload: [
-    "Reload current frame", {
-      repeat: 1
-    }
-  ],
-  reloadTab: [
-    "Reload N tab(s)", {
-      background: true,
-      repeat: 20
-    }
-  ],
-  reloadGivenTab: [
-    "Reload N-th tab", {
-      background: true
-    }
-  ],
-  reopenTab: [
-    "Reopen current page", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  toggleViewSource: [
-    "View page source", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  copyCurrentTitle: [
-    "Copy current tab's title", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  copyCurrentUrl: [
-    "Copy current page's URL (use frame=true)", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  autoCopy: [
-    "Copy selected text or current frame's title or url (use url=true)", {
-      repeat: 1
-    }
-  ],
-  autoOpen: [
-    "Open selected or copied text in a new tab", {
-      repeat: 1
-    }
-  ],
-  searchAs: [
-    "Search selected or copied text using current search engine", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToCopyLinkUrl": [
-    "Copy a link URL to the clipboard", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToCopyLinkText": [
-    "Copy a link text to the clipboard", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToSearchLinkText": [
-    "Open or search a link text", {
-      repeat: 1
-    }
-  ],
-  openCopiedUrlInCurrentTab: [
-    "Open the clipboard's URL in the current tab", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  openCopiedUrlInNewTab: [
-    "Open the clipboard's URL in N new tab(s)", {
-      background: true,
-      repeat: 20
-    }
-  ],
-  enterInsertMode: [
-    "Enter insert mode", {
-      repeat: 1
-    }
-  ],
-  focusInput: [
-    "Focus the first text box on the page. Cycle between them using tab"
-  ],
-  "LinkHints.activateMode": [
-    "Open a link in the current tab", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToOpenInNewTab": [
-    "Open a link in a new tab", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToOpenInNewForegroundTab": [
-    "Open a link in a new tab & switch to it", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeWithQueue": [
-    "Open multiple links in a new tab", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToOpenIncognito": [
-    "Open a link in incognito window", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToDownloadImage": [
-    "Download <img> image", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToOpenImage": [
-    "Show <img> image in new extension's tab", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToDownloadLink": [
-    "Download link url", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToHover": [
-    "select an element and hover", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.activateModeToLeave": [
-    "let mouse leave link", {
-      repeat: 1
-    }
-  ],
-  "LinkHints.unhoverLast": [
-    "Stop hovering at last location", {
-      repeat: 1
-    }
-  ],
-  enterFindMode: [
-    "Enter find mode", {
-      repeat: 1
-    }
-  ],
-  performFind: ["Cycle forward to the next find match"],
-  performBackwardsFind: ["Cycle backward to the previous find match"],
-  switchFocus: [
-    "blur activeElement or refocus it", {
-      repeat: 1
-    }
-  ],
-  simBackspace: [
-    "simulate backspace for once if focused", {
-      repeat: 1
-    }
-  ],
-  goPrevious: [
-    "Follow the link labeled previous or <", {
-      repeat: 1
-    }
-  ],
-  goNext: [
-    "Follow the link labeled next or >", {
-      repeat: 1
-    }
-  ],
-  goBack: [
-    "Go back in history"
-  ],
-  goForward: [
-    "Go forward in history"
-  ],
-  goUp: [
-    "Go up the URL hierarchy", {
-      background: true
-    }
-  ],
-  goToRoot: [
-    "Go to root of current URL hierarchy", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  nextTab: [
-    "Go one tab right", {
-      background: true
-    }
-  ],
-  previousTab: [
-    "Go one tab left", {
-      background: true
-    }
-  ],
-  firstTab: [
-    "Go to the first N-th tab", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  lastTab: [
-    "Go to the last N-th tab", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  createTab: [
-    "Create new tab(s)", {
-      background: true,
-      repeat: 20
-    }
-  ],
-  duplicateTab: [
-    "Duplicate current tab for N times", {
-      background: true,
-      repeat: 20
-    }
-  ],
-  removeTab: [
-    "Close N tab(s)", {
-      background: true,
-      repeat: chrome.sessions.MAX_SESSION_RESULTS
-    }
-  ],
-  removeRightTab: [
-    "Close N-th tab on the right", {
-      background: true
-    }
-  ],
-  restoreTab: [
-    "Restore closed tab(s)", {
-      background: true,
-      repeat: chrome.sessions.MAX_SESSION_RESULTS
-    }
-  ],
-  restoreGivenTab: [
-    "Restore the last N-th tab", {
-      background: true
-    }
-  ],
-  moveTabToNewWindow: [
-    "Move tab to new window", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  moveTabToNextWindow: [
-    "Move tab to next window", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  moveTabToIncognito: [
-    "Make tab in a incognito window", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  togglePinTab: [
-    "Pin/unpin current tab", {
-      background: true,
-      repeat: 20
-    }
-  ],
-  closeTabsOnLeft: [
-    "Close tabs on the left", {
-      background: true
-    }
-  ],
-  closeTabsOnRight: [
-    "Close tabs on the right", {
-      background: true
-    }
-  ],
-  closeOtherTabs: [
-    "Close all other tabs", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  moveTabLeft: [
-    "Move tab to the left", {
-      background: true
-    }
-  ],
-  moveTabRight: [
-    "Move tab to the right", {
-      background: true
-    }
-  ],
-  enableCSTemp: [
-    "enable the site's CS temporarily (use type)", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  toggleCS: [
-    "turn on/off the site's CS (use type)", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  clearCS: [
-    "clear extension's content settings (use type)", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activate": [
-    "Open URL, bookmark, or history entry", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateInNewTab": [
-    "Open URL, bookmark, history entry, in a new tab", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateTabSelection": [
-    "Search through your open tabs", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateBookmarks": [
-    "Open a bookmark", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateBookmarksInNewTab": [
-    "Open a bookmark in a new tab", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateHistory": [
-    "Open a history", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateHistoryInNewTab": [
-    "Open a history in a new tab", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateEditUrl": [
-    "Edit & open selected text or current URL", {
-      repeat: 1
-    }
-  ],
-  "Vomnibar.activateEditUrlInNewTab": [
-    "Edit & open selected text or current URL in new tab", {
-      repeat: 1
-    }
-  ],
-  nextFrame: [
-    "Cycle forward to the next frame on the page", {
-      background: true
-    }
-  ],
-  mainFrame: [
-    "Select the tab's main/top frame", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  "Marks.activateCreateMode": [
-    "Create a new mark", {
-      repeat: 1
-    }
-  ],
-  "Marks.activateGotoMode": [
-    "Go to a mark", {
-      repeat: 1
-    }
-  ],
-  "Marks.clearLocal": [
-    "Remove all local marks for this site", {
-      repeat: 1
-    }
-  ],
-  clearGlobalMarks: [
-    "Remove all global marks", {
-      background: true,
-      repeat: 1
-    }
-  ],
-  openUrl: [
-    "open url (use url: string, newTab: bool)", {
-      background: true,
-      repeat: 20
-    }
-  ],
-  focusOrLaunch: [
-    'focus a tab with arg "url" or open it', {
-      background: true,
-      repeat: 1
-    }
-  ]
+  showHelp: [ "Show help", 1 ],
+  debugBackground: [ "Debug the background page", 1, true ],
+  blank: [ "Do nothing", 1, true ],
+  toggleLinkHintCharacters: [ "Toggle the other link hints (use value)", 1 ],
+  toggleSwitchTemp: [ "Toggle switch only in currnet page (use key[, value])", 1 ],
+  scrollDown: [ "Scroll down" ],
+  scrollUp: [ "Scroll up" ],
+  scrollLeft: [ "Scroll left" ],
+  scrollRight: [ "Scroll right" ],
+  scrollPxDown: [ "Scroll 1px down" ],
+  scrollPxUp: [ "Scroll 1px up" ],
+  scrollPxLeft: [ "Scroll 1px left" ],
+  scrollPxRight: [ "Scroll 1px right" ],
+  scrollToTop: [ "Scroll to the top of the page", 1 ],
+  scrollToBottom: [ "Scroll to the bottom of the page", 1 ],
+  scrollToLeft: [ "Scroll all the way to the left", 1 ],
+  scrollToRight: [ "Scroll all the way to the right", 1 ],
+  scrollPageDown: [ "Scroll a page down" ],
+  scrollPageUp: [ "Scroll a page up" ],
+  scrollFullPageDown: [ "Scroll a full page down" ],
+  scrollFullPageUp: [ "Scroll a full page up" ],
+  reload: [ "Reload current frame", 1 ],
+  reloadTab: [ "Reload N tab(s)", 20, true ],
+  reloadGivenTab: [ "Reload N-th tab", 0, true ],
+  reopenTab: [ "Reopen current page", 1, true ],
+  toggleViewSource: [ "View page source", 1, true ],
+  copyCurrentTitle: [ "Copy current tab's title", 1, true ],
+  copyCurrentUrl: [ "Copy current page's URL (use frame=true)", 1, true ],
+  autoCopy: [ "Copy selected text or current frame's title or url (use url=true)", 1 ],
+  autoOpen: [ "Open selected or copied text in a new tab", 1 ],
+  searchAs: [ "Search selected or copied text using current search engine", 1 ],
+  "LinkHints.activateModeToCopyLinkUrl": [ "Copy a link URL to the clipboard", 1 ],
+  "LinkHints.activateModeToCopyLinkText": [ "Copy a link text to the clipboard", 1 ],
+  "LinkHints.activateModeToSearchLinkText": [ "Open or search a link text", 1 ],
+  openCopiedUrlInCurrentTab: [ "Open the clipboard's URL in the current tab", 1, true ],
+  openCopiedUrlInNewTab: [ "Open the clipboard's URL in N new tab(s)", 20, true ],
+  enterInsertMode: [ "Enter insert mode", 1 ],
+  focusInput: [ "Focus the first text box on the page. Cycle between them using tab" ],
+  "LinkHints.activateMode": [ "Open a link in the current tab", 1 ],
+  "LinkHints.activateModeToOpenInNewTab": [ "Open a link in a new tab", 1 ],
+  "LinkHints.activateModeToOpenInNewForegroundTab": [ "Open a link in a new tab & switch to it", 1 ],
+  "LinkHints.activateModeWithQueue": [ "Open multiple links in a new tab", 1 ],
+  "LinkHints.activateModeToOpenIncognito": [ "Open a link in incognito window", 1 ],
+  "LinkHints.activateModeToDownloadImage": [ "Download <img> image", 1 ],
+  "LinkHints.activateModeToOpenImage": [ "Show <img> image in new extension's tab", 1 ],
+  "LinkHints.activateModeToDownloadLink": [ "Download link url", 1 ],
+  "LinkHints.activateModeToHover": [ "select an element and hover", 1 ],
+  "LinkHints.activateModeToLeave": [ "let mouse leave link", 1 ],
+  "LinkHints.unhoverLast": [ "Stop hovering at last location", 1 ],
+  enterFindMode: [ "Enter find mode", 1 ],
+  performFind: [ "Cycle forward to the next find match" ],
+  performBackwardsFind: [ "Cycle backward to the previous find match" ],
+  switchFocus: [ "blur activeElement or refocus it", 1 ],
+  simBackspace: [ "simulate backspace for once if focused", 1 ],
+  goPrevious: [ "Follow the link labeled previous or <", 1 ],
+  goNext: [ "Follow the link labeled next or >", 1 ],
+  goBack: [ "Go back in history" ],
+  goForward: [ "Go forward in history" ],
+  goUp: [ "Go up the URL hierarchy", 0, true ],
+  goToRoot: [ "Go to root of current URL hierarchy", 1, true ],
+  nextTab: [ "Go one tab right", 0, true ],
+  previousTab: [ "Go one tab left", 0, true ],
+  firstTab: [ "Go to the first N-th tab", 1, true ],
+  lastTab: [ "Go to the last N-th tab", 1, true ],
+  createTab: [ "Create new tab(s)", 20, true ],
+  duplicateTab: [ "Duplicate current tab for N times", 20, true ],
+  removeTab: [ "Close N tab(s)", chrome.sessions.MAX_SESSION_RESULTS, true ],
+  removeRightTab: [ "Close N-th tab on the right", 0, true ],
+  restoreTab: [ "Restore closed tab(s)", chrome.sessions.MAX_SESSION_RESULTS, true ],
+  restoreGivenTab: [ "Restore the last N-th tab", 0, true ],
+  moveTabToNewWindow: [ "Move tab to new window", 1, true ],
+  moveTabToNextWindow: [ "Move tab to next window", 1, true ],
+  moveTabToIncognito: [ "Make tab in a incognito window", 1, true ],
+  togglePinTab: [ "Pin/unpin current tab", 20, true ],
+  closeTabsOnLeft: [ "Close tabs on the left", 0, true ],
+  closeTabsOnRight: [ "Close tabs on the right", 0, true ],
+  closeOtherTabs: [ "Close all other tabs", 1, true ],
+  moveTabLeft: [ "Move tab to the left", 0, true ],
+  moveTabRight: [ "Move tab to the right", 0, true ],
+  enableCSTemp: [ "enable the site's CS temporarily (use type)", 1, true ],
+  toggleCS: [ "turn on/off the site's CS (use type)", 1, true ],
+  clearCS: [ "clear extension's content settings (use type)", 1, true ],
+  "Vomnibar.activate": [ "Open URL, bookmark, or history entry", 1 ],
+  "Vomnibar.activateInNewTab": [ "Open URL, bookmark, history entry, in a new tab", 1 ],
+  "Vomnibar.activateTabSelection": [ "Search through your open tabs", 1 ],
+  "Vomnibar.activateBookmarks": [ "Open a bookmark", 1 ],
+  "Vomnibar.activateBookmarksInNewTab": [ "Open a bookmark in a new tab", 1 ],
+  "Vomnibar.activateHistory": [ "Open a history", 1 ],
+  "Vomnibar.activateHistoryInNewTab": [ "Open a history in a new tab", 1 ],
+  "Vomnibar.activateEditUrl": [ "Edit & open selected text or current URL", 1 ],
+  "Vomnibar.activateEditUrlInNewTab": [ "Edit & open selected text or current URL in new tab", 1 ],
+  nextFrame: [ "Cycle forward to the next frame on the page", 0, true ],
+  mainFrame: [ "Select the tab's main/top frame", 1, true ],
+  "Marks.activateCreateMode": [ "Create a new mark", 1 ],
+  "Marks.activateGotoMode": [ "Go to a mark", 1 ],
+  "Marks.clearLocal": [ "Remove all local marks for this site", 1 ],
+  clearGlobalMarks: [ "Remove all global marks", 1, true ],
+  openUrl: [ "open url (use url: string, newTab: bool)", 20, true ],
+  focusOrLaunch: [ 'focus a tab with arg "url" or open it', 1, true ]
 });
