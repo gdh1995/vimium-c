44c0e8a1cede49e82f1a6136c4ca47467371bd16
gdh1995 <gdh1995@qq.com>
2015-10-27 23:35:50 +0800

    LinkHints.alphabetHints: think <shift+5> as `5` when matching hints
    and shift can switch mode only

--- content/link_hints.js
+++ content/link_hints.js
@@ -606,7 +606,7 @@ LinkHints.alphabetHints = {
     } else if (key === KeyCodes.space) {
       keyStatus.known = 1;
       return [];
-    } else if (keyChar = KeyboardUtils.getKeyChar(event, event.shiftKey)) {
+    } else if (keyChar = KeyboardUtils.getKeyChar(event, false)) {
       if (this.chars.indexOf(keyChar) === -1) {
         keyStatus.known = 1;
         return [];
