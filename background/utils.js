39d2ab3a00f14c9c239dafb397a1168c8b55745d
gdh1995 <gdh1995@qq.com>
2016-04-28 13:31:53 +0800

    add urls like v://policy

--- background/utils.js
+++ background/utils.js
@@ -226,6 +226,9 @@ var exports = {}, Utils = {
     license: "https://raw.githubusercontent.com/gdh1995/vimium-plus/master/MIT-LICENSE.txt",
     popup: "options.html",
     readme: "https://github.com/gdh1995/vimium-plus/blob/master/README.md",
+    permissions: "https://github.com/gdh1995/vimium-plus/blob/master/PRIVACY-POLICY.md#permissions-required",
+    policy: "https://github.com/gdh1995/vimium-plus/blob/master/PRIVACY-POLICY.md",
+    privacy: "https://github.com/gdh1995/vimium-plus/blob/master/PRIVACY-POLICY.md#privacy-policy",
     settings: "options.html",
     __proto__: null
   },
