cd9c2583eb173e9d11c3c043474d8a50d2e0ab2f
gdh1995 <gdh1995@qq.com>
2016-05-11 12:50:37 +0800

    FindMode will not work on a <svg> document

--- content/mode_find.js
+++ content/mode_find.js
@@ -22,6 +22,7 @@ var VFindMode = {
 height:12px;line-height:12px;margin:0;overflow-y:hidden;white-space:nowrap;}\
 body{display:inline;margin-left:1px;}body *{display:inline;}body br{display:none;}',
   activate: function(count, options) {
+    if (!document.head) { return false; }
     if (this.isActive) {
       this.input.focus();
       this.box.contentWindow.getSelection().selectAllChildren(this.input);
