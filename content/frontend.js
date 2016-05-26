ca68bfeae791a9f2db1bd575e50ddaed9af3998d
gdh1995 <gdh1995@qq.com>
2016-05-26 21:20:18 +0800

    noop

--- content/frontend.js
+++ content/frontend.js
@@ -785,7 +785,6 @@ var Settings, VHUD, MainPort, VEventMode;
       else if (window.innerWidth < 3 || window.innerHeight < 3
         || document.body instanceof HTMLFrameSetElement) {
         mainPort.port.postMessage({
-          frameId: request.frameId,
           handler: "nextFrame"
         });
         return;
@@ -916,7 +915,6 @@ opacity:1;pointer-events:none;position:fixed;top:0;width:100%;z-index:2147483647
       KeyboardUtils.onMac = request.onMac;
       r.refreshKeyMappings(request);
       r.reset(request);
-      InsertMode.loading = false;
       r.init = null;
     },
     reset: function(request) {
