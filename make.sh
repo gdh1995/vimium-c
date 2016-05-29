920480375d8705bda694d674ae99a2db1282942f
gdh1995 <gdh1995@qq.com>
2016-05-29 23:01:18 +0800

    rewrite make.sh to produce standard crx file

--- make.sh
+++ make.sh
@@ -1,18 +1,56 @@
 #!/bin/sh
 set -o noglob
 
-if [ -z "$1" ]; then
-OUTPUT=vimium-plus_1.53.zip
+ver=1.53
+output="$1"
+if [ -n "$output" -a -f "$output" ]; then :
 else
-OUTPUT="$1"
-fi
-shift
-
-if [ "$OUTPUT" != "-" -a -f "$OUTPUT" ]; then
-ARGS="-u $*"
-else
-ARGS="$*"
+  if [ -n "$output" -a -d "output" ]; then
+    output="${output/\//}/"
+  fi
+  output="${output}"vimium_plus_$ver.zip
 fi
 
-zip -roX -MM $ARGS "$OUTPUT" . -x ".*" "*.sh" "weidu/*" "test*" \
+args=""
+if [ "$output" != "-" -a -f "$output" ]; then
+  args="-u $args"
+fi
+
+zip -roX -MM $args "$output" . -x ".*" "*.sh" "weidu/*" "test*" \
   "*/.*" "*.zip" "*.crx"
+echo ""
+echo "Wrote $output"
+
+key="$2"
+if [ -z "$key" ]; then
+  echo "No crx key info. Exit."
+  exit
+fi
+if ! which xxd >/dev/null 2>&1 ; then
+  echo 'No "xxd" program. Exit.' 1>&2
+fi
+crx="$3"
+if [ -z "$crx" ]; then
+  crx="${output/%.zip/}.crx"
+fi
+
+openssl sha1 -sha1 -binary -sign "$key" < "$output" > "$crx.sig"
+openssl rsa -pubout -outform DER < "$key" > "$crx.pub" 2>/dev/null
+
+function byte_swap() {
+  echo "${1:6:2}${1:4:2}${1:2:2}${1:0:2}"
+}
+
+crmagic_hex="4372 3234"  # Cr24
+version_hex="0200 0000"  # 2
+pub_len_hex=$(printf '%08x' $(\ls -l "$crx.pub" | awk '{print $5}'))
+pub_len_hex=$(byte_swap $pub_len_hex)
+sig_len_hex=$(printf '%08x' $(\ls -l "$crx.sig" | awk '{print $5}'))
+sig_len_hex=$(byte_swap $sig_len_hex)
+
+(
+  echo "$crmagic_hex $version_hex $pub_len_hex $sig_len_hex" | xxd -r -p
+  cat "$crx.pub" "$crx.sig" "$output"
+) > "$crx"
+echo "Wrote $crx"
+rm -f "$crx.sig" "$crx.pub" 2>/dev/null
