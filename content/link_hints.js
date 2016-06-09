e8d3541c00a38eabae657b95ea53f46901d5e3d7
gdh1995 <gdh1995@qq.com>
2016-06-10 02:43:06 +0800

    if an image has a special cursor, then it's most likely to be clickable

--- content/link_hints.js
+++ content/link_hints.js
@@ -220,9 +220,11 @@ var LinkHints = {
         s = s.replace(LinkHints.hashRe, "").replace(LinkHints.quoteRe, '\\"');
         DomUtils.getClientRectsForAreas(this, arr[0], document.querySelector('map[name="' + s + '"]'));
       }
-      isClickable = LinkHints.mode >= 128 && LinkHints.mode <= LinkHints.CONST.LEAVE
-        || (s = getComputedStyle(element).cursor) && s.indexOf("zoom") >= 0
-        || null;
+      if (LinkHints.mode >= 128 && LinkHints.mode <= LinkHints.CONST.LEAVE
+        || (s = getComputedStyle(element).cursor)
+          && (s.indexOf("zoom") >= 0 || s === "pointer" || s.startsWith("url"))) {
+        isClickable = true;
+      }
       break;
     case "div": case "ul": case "pre": case "ol":
       isScrollable = LinkHints.getScrollable(element);
