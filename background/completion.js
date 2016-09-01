24c76f964a712bab5e836d715f73ce26f87cef68
gdh1995 <gdh1995@qq.com>
2016-09-01 20:53:41 +0800

    decode urls shorter than 400 chars

--- background/completion.js
+++ background/completion.js
@@ -940,7 +940,7 @@ searchEngines: {
         try {
           while (++i < l) {
             j = a[i], s = j.url;
-            if (s.length >= 200) { j.text = s; continue; }
+            if (s.length >= 400) { j.text = s; continue; }
             t = f(s);
             j.text = t !== s ? t : s;
           }
@@ -1004,7 +1004,7 @@ searchEngines: {
   (function() {
     var d = Decoder.dict, f = Decoder._f, t = Decoder.todos;
     Decoder.decodeURL = function(a, o) {
-      if (a.length >= 200) { return a; }
+      if (a.length >= 400) { return a; }
       try {
         return f(a);
       } catch (e) {
