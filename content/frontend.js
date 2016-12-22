55c43f9c7fe5afd2c65824edd11e87df41af3a18
gdh1995 <gdh1995@qq.com>
2016-12-23 00:33:23 +0800

    about:blank injection: exit if contentEditable and delay initing until loaded
    Add an extra waiting after window becomes loaded so that
    the parent frame can finish its initialization works.

--- content/frontend.js
+++ content/frontend.js
@@ -56,7 +56,13 @@ var VSettings, VHUD, VPort, VEventMode;
       port.onMessage.addListener(this.Listener);
     }
   };
-  vPort.connect(1);
+  location.href !== "about:blank" ? vPort.connect(1)
+  : (window.onload = function() { setTimeout(function() {
+    window.onload = null;
+    var a = document.body, exit;
+    exit = !!a && (a.isContentEditable || a.childElementCount === 1 && a.firstElementChild.isContentEditable);
+    exit ? VSettings.destroy(true) : vPort.connect(1);
+  }, 18); });
   VPort = { post: vPort.post, send: vPort.send };
 
   VSettings = {
