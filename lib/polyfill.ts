c3c9a5d59811a7c7bb11f0a4a536671ac52c6eb2
gdh1995 <gdh1995@qq.com>
2016-12-30 01:08:36 +0800

    fix a bug in polyfill
    Old code used `valueOf` to convert an object to string, which is wrong.
    
    According to tests, the code below should return `[true, false]`.
    ```
    var a = {toString: i=>'1', valueOf: i=>2};
    ['1'.startsWith(a), '2'.startsWith(a)]
    ```

--- lib/polyfill.ts
+++ lib/polyfill.ts
@@ -13,6 +13,7 @@ interface String {
     let err = check(this, searchString, "startsWith");
     if (err !== null) { throw new TypeError(err); }
     let a = "" + this, b = "" + searchString, c = +arguments[1];
+    a = String(this), b = String(searchString);
     c = c > 0 ? Math.min(c | 0, a.length) : 0;
     return a.lastIndexOf(b, c) === c;
   };
@@ -21,8 +22,9 @@ interface String {
   String.prototype.endsWith = function endsWith(this: ObjectCoercible, searchString: anyNotSymbol): boolean {
     let err = check(this, searchString, "endsWith");
     if (err !== null) { throw new TypeError(err); }
-    let a = "" + this, b = "" + searchString, p: any = arguments[1], c = +p, l = a.length, u: undefined;
-    c = (p === u ? l : c > 0 ? Math.min(c | 0, l) : 0) - b.length;
+    let a = "" + this, b = "" + searchString, p: any = arguments[1], c = +p, u: undefined;
+    a = String(this), b = String(searchString);
+    c = (p === u ? a.length : c > 0 ? Math.min(c | 0, a.length) : 0) - b.length;
     return c >= 0 && a.indexOf(b, c) === c;
   };
 
