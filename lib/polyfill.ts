0eba2193a36d07292aea1dc9334b48518424ad40
gdh1995 <gdh1995@qq.com>
2016-12-29 23:55:46 +0800

    noop: rework polyfill:check

--- lib/polyfill.ts
+++ lib/polyfill.ts
@@ -10,35 +10,27 @@ interface String {
 (function (): void {
   if (String.prototype.startsWith) { return; }
   String.prototype.startsWith = function startsWith(this: ObjectCoercible, searchString: anyNotSymbol): boolean {
-    let err = check(this, searchString);
-    if (err !== null) { throw new TypeError(err("startsWith")); }
+    let err = check(this, searchString, "startsWith");
+    if (err !== null) { throw new TypeError(err); }
     let a = "" + this, b = "" + searchString, c = +arguments[1];
-    c = c > 0 ? Math.min(c, a.length) | 0 : 0;
+    c = c > 0 ? Math.min(c | 0, a.length) : 0;
     return a.lastIndexOf(b, c) === c;
   };
 
   if (String.prototype.endsWith) { return; }
   String.prototype.endsWith = function endsWith(this: ObjectCoercible, searchString: anyNotSymbol): boolean {
-    let err = check(this, searchString);
-    if (err !== null) { throw new TypeError(err("startsWith")); }
+    let err = check(this, searchString, "endsWith");
+    if (err !== null) { throw new TypeError(err); }
     let a = "" + this, b = "" + searchString, p: any = arguments[1], c = +p, l = a.length, u: undefined;
-    c = (p === u ? l : c > 0 ? Math.min(c, l) | 0 : 0) - b.length;
+    c = (p === u ? l : c > 0 ? Math.min(c | 0, l) : 0) - b.length;
     return c >= 0 && a.indexOf(b, c) === c;
   };
 
-  function check(a: any, b: any): null | ((name: string) => string) {
-    if (a == null) { return errNullThis; }
+  function check(a: any, b: any, func: string): null | string {
+    if (a == null) { return `String.prototype.${func} called on null or undefined`; }
     if (!b) { return null; }
     let i: any, f: any, u: undefined;
     i = typeof Symbol === "function" && Symbol.match && (f = b[Symbol.match]) !== u ? f : b instanceof RegExp;
-    return i ? errSearchRegExp : null;
-  }
-
-  function errNullThis(name: string): string {
-    return `String.prototype.${name} called on null or undefined`;
-  }
-
-  function errSearchRegExp(name: string): string {
-    return `First argument to String.prototype.${name} must not be a regular expression`;
+    return i ? `First argument to String.prototype.${func} must not be a regular expression` : null;
   }
 })();
