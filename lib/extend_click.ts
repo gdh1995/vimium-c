ef3a0c9fd1807f9af79f015e09c4570c07eaf7a7
gdh1995 <gdh1995@qq.com>
2017-02-14 20:33:44 +0800

    ts: add lib/extend_click

--- lib/extend_click.ts
+++ lib/extend_click.ts
@@ -1,6 +1,9 @@
-"use strict";
-(function(func) {
-  var d = document, script = d.createElement("script"), installer, onclick, box;
+/// <reference path="../content/base.d.ts" />
+(function(this: void, func: (this: void) => void): void {
+  type Listener = (this: void, e: Event) => void;
+  let d: Document | Document["documentElement"] = document
+    , script = d.createElement("script") as HTMLScriptElement | Element
+    , installer: Listener | null, onclick: Listener | null, box: EventTarget;
   if (!(script instanceof HTMLScriptElement)) { return; }
   addEventListener("VimiumReg", installer = function(event) {
     removeEventListener("VimiumReg", installer, true);
@@ -9,53 +12,51 @@
     installer = null;
   }, true);
   addEventListener("VimiumOnclick", onclick = function(event) {
-    event.target.vimiumHasOnclick = true;
+    (event.target as Element).vimiumHasOnclick = true;
     event.stopPropagation();
   }, true);
-  /* globals VSettings: false */
-  VSettings.onDestroy = function() {
+  (window as any).VSettings.onDestroy = function() {
     removeEventListener("VimiumReg", installer, true);
     removeEventListener("VimiumOnclick", onclick, true);
     box && box.removeEventListener("VimiumOnclick", onclick, true);
   };
   script.type = "text/javascript";
   script.textContent = '"use strict";(' + func.toString() + ')();';
-  d = d.documentElement || d;
+  d = (d as Document).documentElement || d;
   d.insertBefore(script, d.firstChild).remove();
-})(function() {
-var _listen, box, handler, reg, register, toRegister, timeout;
-
-_listen = EventTarget.prototype.addEventListener;
-toRegister = [];
-register = toRegister.push.bind(toRegister);
-EventTarget.prototype.addEventListener = function(type, listener, useCapture) {
+})(function(this: void): void {
+const _listen = EventTarget.prototype.addEventListener;
+let box: HTMLDivElement, toRegister: Element[] = [],
+register = toRegister.push.bind<Element[], Element, number>(toRegister);
+EventTarget.prototype.addEventListener = function(this: EventTarget, type: string
+    , listener: EventListenerOrEventListenerObject, useCapture?: any) {
   if (type === "click" && this instanceof Element) {
-    register(this);
+    register(this as Element);
   }
   return _listen.call(this, type, listener, useCapture);
 };
 
-handler = function() {
+let handler = function(this: void): void {
   removeEventListener("DOMContentLoaded", handler, true);
   clearTimeout(timeout);
   box = document.createElement("div");
-  document.documentElement.appendChild(box);
+  (document.documentElement as HTMLElement | SVGElement).appendChild(box);
   box.dispatchEvent(new CustomEvent("VimiumReg"));
   box.remove();
   register = reg;
-  for (var i = toRegister.length; 0 <= --i; ) { register(toRegister[i]); }
-  handler = toRegister = reg = null;
+  for (let i of toRegister) { register(i); }
+  handler = toRegister = null as never;
 };
 _listen("DOMContentLoaded", handler, true);
-timeout = setTimeout(handler, 1000);
-
-reg = setTimeout.bind(null, function(element) {
-  var e1, e2, event;
-  event = new CustomEvent("VimiumOnclick");
+const timeout = setTimeout(handler, 1000),
+reg = setTimeout.bind<void, (el: Element) => void, number, Element, number>(null as never,
+function(this: void, element: Element): void {
+  const event = new CustomEvent("VimiumOnclick");
   if (document.contains(element)) {
     element.dispatchEvent(event);
     return;
   }
+  let e1, e2;
   for (e1 = element; (e2 = e1.parentElement) != null; e1 = e2) {}
   if (e1.parentNode != null) { return; }
   // NOTE: ignore nodes belonging to a shadowRoot
