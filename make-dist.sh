8c423d6cc92edfbd192b45cc78db3ec161cdb48e
gdh1995 <gdh1995@qq.com>
2017-02-28 16:26:23 +0800

    make-dist: ignore more files

--- make-dist.sh
+++ make-dist.sh
@@ -3,14 +3,15 @@ set +o noglob
 input=$(echo *)
 set -o noglob
 
-exclude_dir="! -path weidu* ! -path test* ! -path git* ! -path dist*"
+exclude_dir="! -path weidu* ! -path test* ! -path git* ! -path dist* ! -path types*"
 exclude_dir="$exclude_dir ! -path node_modules*"
 
 input=$(find $input $exclude_dir \
-    ! -name todo* ! -name tsconfig* ! -name *~ \
-    ! -name *.crx ! -name *.js ! -name .*.json ! -name *.ts ! -name *.sh \
+    ! -name .* ! -name todo* ! -name tsconfig* ! -name vimium.css ! -name *~ \
+    ! -name *.coffee ! -name *.crx ! -name *.js ! -name *.sh ! -name *.ts \
     ! -name *.zip)
 input2=$(find $input -name *.min.js $exclude_dir)
+input2="$input2 pages/newtab.js"
 
 out_dir=dist
 [ -d "$out_dir" ] || mkdir -p "$out_dir"
@@ -19,12 +20,12 @@ for i in $input $input2; do
   y=$out_dir/$i
   if [ -f "$i" ]; then
     if [ "$i" -nt "$y" ]; then
-      cp -p "$i" "$out_dir/$i"
+      cp -a "$i" "$out_dir/$i"
       echo "copy: $i"
     fi
   elif [ ! -e "$out_dir/$i" ]; then
     mkdir -p "$out_dir/$i"
-    echo "create folder: $i"
+    echo "mkdir: $i"
   fi
 done
 
