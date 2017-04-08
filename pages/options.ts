b4704531d832c501e7d2c98c1671cd1b46681a22
gdh1995 <gdh1995@qq.com>
2017-04-08 17:32:48 +0800

    fix a typo bug

--- pages/options.ts
+++ pages/options.ts
@@ -367,7 +367,7 @@ interface AdvancedOptBtn extends HTMLButtonElement {
   }
 
   if (bgSettings.CONST.ChromeVersion < BrowserVer.MinWithFrameId) {
-    element = $("VomnibarPage");
+    element = $("vomnibarPage");
     element.title = `Vimium++ can not use a HTTP pages as Vomnibar before Chrome ${BrowserVer.MinWithFrameId}`;
     if ("chrome /front/".indexOf(Option.all.vomnibarPage.previous.substring(0, 6)) === -1) {
       element.style.textDecoration = "line-through";
