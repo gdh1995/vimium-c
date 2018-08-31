9d21aa3f4c703a9f2aa86ed58ba3518515a3699b
Dahan Gong <gdh1995@qq.com>
2018-08-31 08:07:36 +0000

    startsWith polyfill: store used global names

--- lib/polyfill.ts
+++ lib/polyfill.ts
@@ -17,15 +17,17 @@ interface String {
 }
 
 (function (): void {
-  const symMatch = typeof Symbol === "function" && Symbol.match || null;
+  const symMatch = typeof Symbol === "function" && Symbol.match || null,
+  S = String, RE = RegExp, TE = TypeError;
 
   "".startsWith || (
   String.prototype.startsWith = function startsWith(this: ObjectCoercible, searchString: anyNotSymbol): boolean {
     const err = check(this, searchString);
     if (err === true) { return !(this + "" + searchString); }
-    if (err !== null) { throw new TypeError(err.replace("${func}", "startsWith")); }
-    let a = String(this), b = String(searchString), c = +arguments[1];
-    c = c > 0 ? Math.min(c | 0, a.length) : 0;
+    if (err !== null) { throw new TE(err.replace("${func}", "startsWith")); }
+    let a = S(this), b = S(searchString), c = +arguments[1];
+    c = c > 0 ? c | 0 : 0;
+    c > a.length && (c = a.length);
     return a.lastIndexOf(b, c) === c;
   });
 
@@ -33,9 +35,10 @@ interface String {
   String.prototype.endsWith = function endsWith(this: ObjectCoercible, searchString: anyNotSymbol): boolean {
     const err = check(this, searchString);
     if (err === true) { return !(this + "" + searchString); }
-    if (err !== null) { throw new TypeError(err.replace("${func}", "endsWith")); }
-    let a = String(this), b = String(searchString), p: any = arguments[1], c = +p, u: undefined;
-    c = (p === u ? a.length : c > 0 ? Math.min(c | 0, a.length) : 0) - b.length;
+    if (err !== null) { throw new TE(err.replace("${func}", "endsWith")); }
+    let a = S(this), b = S(searchString), p: any = arguments[1], l = a.length, u: undefined,
+    c = (p === u ? l : (c = +p) > 0 ? c | 0 : 0) - b.length;
+    c > l && (c = l);
     return c >= 0 && a.indexOf(b, c) === c;
   });
 
@@ -44,7 +47,7 @@ interface String {
     if (a == null) { return "String.prototype.${func} called on null or undefined"; }
     if (!b) { return null; }
     if (typeof a === "symbol" || typeof b === "symbol") { return true; }
-    let f: any, u: undefined, i = symMatch && (f = b[symMatch]) !== u ? f : b instanceof RegExp;
+    let f: any, u: undefined, i = symMatch && (f = b[symMatch]) !== u ? f : b instanceof RE;
     return i ? "First argument to String.prototype.${func} must not be a regular expression" : null;
   }
 })();
