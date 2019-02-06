2a3fdd6af59aa8bbb34b231d0b74a400d832f132
gdh1995 <gdh1995@qq.com>
2019-02-06 18:36:00 +0800

    Revert "fix a typos"
    This reverts commit 10eed5b9c07d18d0ede66fb705dca363727760ae.

--- background/help_dialog.ts
+++ background/help_dialog.ts
@@ -5,6 +5,7 @@ var HelpDialog = {
     : ChromeVer < BrowserVer.MinFixedCSS$All$MayMistakenlyResetFixedPosition
       && ChromeVer >= BrowserVer.MinCSS$All$MayMistakenlyResetFixedPosition ? "position: fixed;"
     : "",
+  templateEl_: null as HTMLTemplateElement | null,
   render_: (function(this: void, request: FgReq[kFgReq.initHelp]): string {
     if (!HelpDialog.inited_) {
       if (Settings.CONST.StyleCacheId_.split(',', 2)[1].indexOf("s") < 0) {
@@ -17,10 +18,11 @@ var HelpDialog = {
       HelpDialog.inited_ = true;
     }
     Object.setPrototypeOf(request, null);
-    const commandToKeys = Object.create<string[]>(null), ref = CommandsData_.keyToCommandRegistry_,
+    const commandToKeys = Object.create<[string, CommandsNS.Item][]>(null), ref = CommandsData_.keyToCommandRegistry_,
           hideUnbound = !request.unbound, showNames = !!request.names;
     for (const key in ref) {
-      let command = (ref[key] as CommandsNS.Item).command;
+      const registry = ref[key] as NonNullable<(typeof ref)[string]>;
+      let command = registry.command;
       if (command.endsWith(".activateMode")) {
         command = command.substring(0, command.length - 4);
       } else if (command.indexOf("EditUrl") > 0) {
@@ -28,7 +30,7 @@ var HelpDialog = {
       } else if (command === "quickNext") {
         command = "nextTab";
       }
-      (commandToKeys[command] || (commandToKeys[command] = [])).push(key);
+      (commandToKeys[command] || (commandToKeys[command] = [])).push([key, registry]);
     }
     const result = Object.setPrototypeOf({
       version: Settings.CONST.VerName,
@@ -37,35 +39,45 @@ var HelpDialog = {
       tip: showNames ? "Tip: click command names to copy them to the clipboard." : "",
       lbPad: showNames ? '\n\t\t<tr><td class="HelpTd TdBottom">&#160;</td></tr>' : ""
     }, null) as SafeDict<string>;
-    return (<string>Settings.cache.helpDialog).replace(<RegExpSearchable<1>>/\{\{(\w+)}}/g, function(_, group: string) {
+    const html = (<string>Settings.cache.helpDialog).replace(<RegExpSearchable<1>>/\{\{(\w+)}}/g, function(_, group: string) {
       let s = result[group];
       return s != null ? s
         : HelpDialog.groupHtml_(group, commandToKeys, hideUnbound, showNames);
     });
+    HelpDialog.templateEl_ = null;
+    return html;
   }),
-  groupHtml_: (function(this: {}, group: string, commandToKeys: SafeDict<string[]>
+  groupHtml_: (function(this: {}, group: string, commandToKeys: SafeDict<[string, CommandsNS.Item][]>
       , hideUnbound: boolean, showNames: boolean): string {
     const _ref = (this as typeof HelpDialog).commandGroups_[group], renderItem = (this as typeof HelpDialog).commandHtml_
       , availableCommands = CommandsData_.availableCommands_ as Readonly<EnsuredSafeDict<CommandsNS.Description>>;
-    let keys: string[] | undefined, html = "";
+    let html = "";
     for (let _i = 0, _len = _ref.length; _i < _len; _i++) {
       const command = _ref[_i];
-      keys = commandToKeys[command];
+      let keys = commandToKeys[command];
       if (hideUnbound && !keys) { continue; }
+      const isAdvanced = command in (this as typeof HelpDialog).advancedCommands_
+        , description = availableCommands[command][0];
       let klen = -2, bindings = '';
       if (keys && keys.length > 0) {
         bindings = '\n\t\t<span class="HelpKey">';
-        for (const key of keys) {
+        for (const item of keys) {
+          const help = item[1].help as Partial<CommandsNS.NormalizedCustomHelpInfo> | null;
+          help && (this as typeof HelpDialog).normalizeHelpInfo_(help);
+          const key = help && help.$key || Utils.escapeText_(item[0]);
+          if (help && help.$desp) {
+            let singleBinding = `\n\t\t<span class="HelpKey">${key}</span>\n\t`;
+            html += renderItem(isAdvanced, singleBinding, help.$desp, showNames ? command: "");
+            continue;
+          }
           if (klen >= 0) {
             bindings += '</span>, <span class="HelpKey">';
           }
-          bindings += Utils.escapeText_(key);
-          klen += key.length + 2;
+          bindings += key;
+          klen += item[0].length + 2;
         }
         bindings += '</span>\n\t';
       }
-      const isAdvanced = command in (this as typeof HelpDialog).advancedCommands_
-        , description = availableCommands[command][0];
       if (klen <= 12) {
         html += renderItem(isAdvanced, bindings, description, showNames ? command: "");
       } else {
@@ -94,6 +106,48 @@ var HelpDialog = {
     }
     return html + "</td>\n</tr>\n";
   }),
+  normalizeHelpInfo_ (help: Partial<CommandsNS.NormalizedCustomHelpInfo>): void {
+    if (help.$key != null) { return; }
+    let a = this.templateEl_;
+    a || (a = this.templateEl_ = document.createElement('template'));
+    help.$key = help.key ? this.safeHTML_(help.key, a) : "";
+    help.$desp = help.desp ? this.safeHTML_(help.desp, a) : "";
+  },
+  // https://support.zendesk.com/hc/en-us/articles/115015895948-Allowing-unsafe-HTML-in-articles
+  safeTags: {
+    a: 1, abbr: 1, acronym: 1, address: 1, b: 1, big: 1, blockquote: 1, br: 1,
+    cite: 1, code: 1, colgroup: 1, dd: 1, del: 1, dfn: 1, div: 1, dl: 1, dt: 1,
+    em: 1, h1: 1, h2: 1, h3: 1, h4: 1, h5: 1, h6: 1, hr: 1, i: 1, id: 1, img: 1,
+    ins: 1, kbd: 1, li: 1, ol: 1, p: 1, pre: 1, samp: 1, small: 1, span: 1,
+    strong: 1, sub: 1, sup: 1, table: 1, tbody: 1, td: 1, tfoot: 1, th: 1,
+    thead: 1, tr: 1, tt: 1, u: 1, ul: 1, var: 1,
+    __proto__: null as never
+  } as SafeEnum,
+  safeHTML_ (raw: string, parent: HTMLElement): string {
+    parent.innerHTML = raw;
+    for (let arr = parent.querySelectorAll("*"), i = 0, end = arr.length; i < end; i++) {
+      const el = arr[i];
+      if (el instanceof HTMLFormElement || el instanceof HTMLFrameSetElement) {
+        el.remove();
+        continue;
+      }
+      if (!((el.tagName + "").toLowerCase() in this.safeTags) && !(el instanceof HTMLUnknownElement)) {
+        el.remove();
+        continue;
+      }
+      const attrsToRemove = [] as Attr[];
+      for (let attrs = el.attributes, len2 = attrs.length, j = 0; j < len2; j++) {
+        const attrName = attrs[j].name.toLowerCase();
+        if (attrName.startsWith("on") || attrName.indexOf(":on") >= 0 || attrName.endsWith("href")) {
+          attrsToRemove.push(attrs[j]);
+        }
+      }
+      for (let j = 0; j < attrsToRemove.length; j++) {
+        el.removeAttributeNode(attrsToRemove[j]);
+      }
+    }
+    return parent.innerHTML;
+  },
   commandGroups_: { __proto__: null as never,
     pageNavigation: ["scrollDown", "scrollUp", "scrollLeft", "scrollRight", "scrollToTop"
       , "scrollToBottom", "scrollToLeft", "scrollToRight", "scrollPageDown", "scrollPageUp"
@@ -118,7 +172,7 @@ var HelpDialog = {
       , "Vomnibar.activateBookmarks", "Vomnibar.activateBookmarksInNewTab", "Vomnibar.activateHistory"
       , "Vomnibar.activateHistoryInNewTab", "Vomnibar.activateTabSelection"
       , "Vomnibar.activateUrl", "Vomnibar.activateUrlInNewTab"
-      , "LinkHints.activateModeToOpenVomnibar", "toggleVomnibarStyle"],
+      , "LinkHints.activateModeToOpenVomnibar"],
     historyNavigation: ["goBack", "goForward", "reopenTab"],
     findCommands: ["enterFindMode", "performFind", "performBackwardsFind", "clearFindHistory"],
     tabManipulation: ["nextTab", "previousTab", "firstTab", "lastTab", "createTab", "duplicateTab"
