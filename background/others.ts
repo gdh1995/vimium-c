gdh1995/vimium-c

commit 031d69cdddd8381f7c4c004ee0f15589253c0447

if setting storage is empty, set newTabUrl to its options page
It should be easier to use for new users.

background/settings.ts
@@ -398,6 +398,9 @@ chrome.runtime.getPlatformInfo ? chrome.runtime.getPlatformInfo(function(info): 
   }
   obj.ContentScripts_ = ref2.map(func);
 
+  if (localStorage.length <= 0) {
+    Settings.set("newTabUrl", "pages/options.html#!newTabUrl");
+  }
   const hasAll = "all" in (document.documentElement as HTMLElement).style;
   obj.StyleCacheId_ = obj.VerCode + "," + ChromeVer
     + (window.ShadowRoot ? "s" : "") + (hasAll ? "a" : "") + ",";

commit fc5d041ba4564297ffc8a029c41943fc67b4f4f6

re-download synced settings on first installing
The regression is imported in commit 031d69cdddd8381f7c4c004ee0f15589253c0447

background/others.ts
@@ -6,7 +6,8 @@ declare const enum OmniboxData {
   PreservedTitle = 16,
 }
 
-// Note: if localStorage is cleaned (considering newTabUrl_f, innerCSS, findCSS, omniCSS),
+// Note: if localStorage is cleaned
+//       (considering: newTabUrl (the alerting one), newTabUrl_f, innerCSS, findCSS, omniCSS),
 //       try to get vimSync from storage.sync
 setTimeout(function() {
   type SettingsToUpdate = {
@@ -118,7 +119,8 @@ setTimeout(function() {
     }
   };
   const sync1 = Settings.get("vimSync");
-  if (sync1 === false || (!sync1 && localStorage.length > 4)) {
+  if (sync1 === false || (!sync1 && (localStorage.length > 5
+                                    || Settings.get("newTabUrl") !== Settings.CONST.NewTabForNewUser_))) {
     return;
   }
   if (!storage()) { return; }

background/settings.ts
@@ -341,6 +341,7 @@ w|wiki:\\\n  https://www.wikipedia.org/w/index.php?search=%s Wikipedia
       helpDialog: "/front/help_dialog.html"
     },
     InjectEnd_: "content/injected_end.js",
+    NewTabForNewUser_: "pages/options.html#!newTabUrl",
     OptionsPage: "pages/options.html", Platform: "", PolyFill_: "lib/polyfill.js",
     RedirectedUrls_: {
       about: "https://github.com/gdh1995/vimium-c",
@@ -399,7 +400,7 @@ chrome.runtime.getPlatformInfo ? chrome.runtime.getPlatformInfo(function(info): 
   obj.ContentScripts_ = ref2.map(func);
 
   if (localStorage.length <= 0) {
-    Settings.set("newTabUrl", "pages/options.html#!newTabUrl");
+    Settings.set("newTabUrl", Settings.CONST.NewTabForNewUser_);
   }
   const hasAll = "all" in (document.documentElement as HTMLElement).style;
   obj.StyleCacheId_ = obj.VerCode + "," + ChromeVer