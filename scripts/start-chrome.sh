6e4199baa214ae2405b9ba9ebed2c1211968dc70
gdh1995 <gdh1995@qq.com>
2019-02-15 23:02:13 +0800

    start-chrome: update paths

--- scripts/start-chrome.sh
+++ scripts/start-chrome.sh
@@ -3,12 +3,13 @@
 VER=
 FLAGS=
 OTHER_EXT=
+OTHER_ARGS=
 GUD=/r/TEMP/GUD
 WORKING_DIR=/r/working
 
 while [[ $# -gt 0 ]]; do
 case "$1" in
-  clean|--clean)
+  clean|ckean|--clean)
     if test -d "$GUD"; then
       rm -rf "$GUD" || exit $?
       dir=${GUD}; dir=${dir#/}; gud_w=${dir%%/*}; dir=${dir#[a-z]}
@@ -23,6 +24,10 @@ case "$1" in
     ;;
   leg|legacy|--legacy)
     FLAGS=$FLAGS" --disable-javascript-harmony-shipping"
+    shift
+    ;;
+  test|--test) # no the "Disable developer mode extensions" dialog, but add an extra infobar
+    OTHER_ARGS=$OTHER_ARGS" --enable-automation"
     shift
     ;;
   only|--only)
@@ -52,12 +57,14 @@ else
   dir=${dir%/*}; dir=${dir%/*}; dir=${dir#/}; VC_EXT=${dir%%/*}; dir=${dir#[a-z]}
   VC_EXT=${VC_EXT^}:${dir}
 fi
+test "$VER" == cur && VER=
 if test "$VER" == wo; then
   EXE=$WORKING_DIR/Chrome-bin/chrome.exe
 else
-  EXE=$CHROME_ROOT/${VER:-Chrome}/chrome.exe
+  EXE=$WORKING_DIR/${VER:-cur}/chrome.exe
+  test -f "$EXE" || EXE=$CHROME_ROOT/${VER:-Chrome}/chrome.exe
 fi
-if test "$VER" == wo || test ${VER:-99} -ge 45; then
+if test "$VER" == wo -o "$VER" == prev || test ${VER:-99} -ge 45; then
   ub=${VC_EXT}/../uBlock/dist/build/uBlock0.chromium
   test -d "$ub" && OTHER_EXT=${OTHER_EXT},${ub}
 fi
@@ -68,13 +75,18 @@ if ! test -f "$EXE"; then
   exit 1
 fi
 
+dir=${GUD}; dir=${dir#/}; gud_w=${dir%%/*}; dir=${dir#[a-z]}
+gud_w=${gud_w^}:${dir}
+
 test -d "$WORKING_DIR" && cd "$WORKING_DIR" 2>/dev/null || cd "${EXE%/*}"
 
 # Refer: https://peter.sh/experiments/chromium-command-line-switches/
-echo -E Run: "${exe_w}" at ${GUD} with "${VC_EXT}"
+echo -E Run: "${exe_w}" at ${gud_w} with "${VC_EXT}"
 $RUN "$EXE" \
-  --user-data-dir=${GUD} \
+  --user-data-dir=${gud_w} \
   --load-extension=${VC_EXT}${OTHER_EXT} \
   --homepage chrome-extension://hfjbmagddngcpeloejdejnfgbamkjaeg/pages/options.html \
   --disable-office-editing-component-extension \
+  --disable-extensions-file-access-check \
+  $OTHER_ARGS \
   --start-maximized $FLAGS "$@"
