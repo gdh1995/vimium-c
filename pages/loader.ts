ec2d3aa7c769fe1245b0a4f1dda3d8ebd7033b92
gdh1995 <gdh1995@qq.com>
2019-02-27 00:31:58 +0800

    debug: add a shortcut to reload CSS

--- pages/loader.ts
+++ pages/loader.ts
@@ -1,6 +1,11 @@
 /// <reference no-default-lib="true"/>
 /// <reference path="../types/base/index.d.ts" />
 /// <reference path="../types/vimium_c.d.ts" />
+/// <reference path="../background/bg.d.ts" />
+/// <reference path="../background/settings.ts" />
+interface BgWindow extends Window {
+  Settings: typeof Settings;
+}
 declare var browser: unknown;
 var VimiumInjector: VimiumInjector | undefined | null = null;
 if (typeof browser !== "undefined" && browser && (browser as any).runtime) {
@@ -22,5 +27,12 @@ window.chrome && chrome.runtime && chrome.runtime.getManifest && (function() {
   scripts[scripts.length - 1].onload = function(): void {
     for (let i = scripts.length; 0 <= --i; ) { scripts[i].remove(); }
     (window as any).VDom && ((window as any).VDom.Scripts_ = false);
+  };
+  if (typeof NDEBUG === "undefined" || !NDEBUG)
+  (window as {} as {updateCSS(): void}).updateCSS = function(): void {
+    const settings = (chrome.extension.getBackgroundPage() as BgWindow).Settings;
+    settings.fetchFile_("baseCSS", function(): void {
+      settings.postUpdate_("userDefinedCss");
+    });
   };
 })();
