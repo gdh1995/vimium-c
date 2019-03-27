85e9fd659619186cfade759571fad74add35551f
gdh1995 <gdh1995@qq.com>
2019-03-27 12:36:33 +0800

    css color format: use #rrggbbaa if possible

--- background/settings.ts
+++ background/settings.ts
@@ -166,6 +166,16 @@ var Settings = {
           && browserVer < BrowserVer.MinSpecCompliantShadowBlurRadius) {
         css = css.replace("3px 5px", "3px 7px");
       }
+      if ((Build.BTypes & (BrowserType.Chrome | BrowserType.Edge) && Build.MinCVer < BrowserVer.MinCSS$Color$$RRGGBBAA
+          && ((!(Build.BTypes & ~BrowserType.Edge) || Build.BTypes & BrowserType.Edge && OnOther === BrowserType.Edge)
+            || ChromeVer < BrowserVer.MinCSS$Color$$RRGGBBAA
+          ))) {
+        css = css.replace(<RegExpG & RegExpSearchable<0>> /#[0-9a-z]{8}/gi, function(s: string): string {
+          const color = parseInt(s.slice(1), 16),
+          r = color >>> 24, g = (color >> 16) & 0xff, b = (color >> 8) & 0xff, a = (color & 0xff) / 255 + "";
+          return `rgba(${r},${g},${b},${a.slice(0, 4)})`;
+        });
+      }
       if ((Build.BTypes & ~BrowserType.Chrome || Build.MinCVer < BrowserVer.MinShadowDOMV0)
           && browserInfo.indexOf("s") < 0) {
         // Note: &vimium.min.css: this requires `:host{` is at the beginning
