5da787afb5d024ec3508216a39236ef552fa6a7a
gdh1995 <gdh1995@qq.com>
2019-04-30 19:56:31 +0800

    noop

--- scripts/make.sh
+++ scripts/make.sh
@@ -98,6 +98,7 @@ fi
 zip -rX -MM $args "$output_for_zip" ${input[@]} -x 'weidu*' 'test*' 'git*' \
   'dist*' 'front/vimium.css' 'node_modules*' 'script*' '*tsconfig*' 'type*' \
   'pages/dialog_ui*' 'GUD*' 'Gulp*' 'gulp*' 'package*' 'todo*' 'tsc.*' \
+  '*tslint*' \
   '*.coffee' '*.crx' '*.enc' '*.log' '*.sh' '*.ts' '*.zip' $ZIP_IGNORE $4
 err=$?
 [ $pushd_err -eq 0 ] && popd >/dev/null 2>&1
