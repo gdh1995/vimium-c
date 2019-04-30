a5b3395a256d91a513ea6c4fa6a82f3dfcf9f47a
gdh1995 <gdh1995@qq.com>
2019-04-30 18:35:54 +0800

    build: simpler tsc.js

--- scripts/tsc.js
+++ scripts/tsc.js
@@ -8,29 +8,19 @@ if (/\bnode\b/i.test(argv[argi])) {
 if (/\btsc(\.\b|$)/i.test(argv[argi])) {
   argi++;
 }
-var cwd = popProcessArg(argi);
-cwd === "." && (cwd = null);
-
-if (cwd && !fs.existsSync("package.json")) {
-  process.chdir(__dirname);
+var cwd = argv[argi];
+argv.length = argi;
+if (cwd && fs.existsSync(cwd) && fs.statSync(cwd).isDirectory()) {
+  process.chdir(cwd);
+  cwd = null;
+} else if (cwd) {
+  console.error("No such command or directory:", cwd);
+  return;
 }
-(function() {
-  if (cwd && fs.existsSync(cwd) && fs.statSync(cwd).isDirectory()) {
-    process.chdir(cwd);
-    cwd = null;
-  } else if (cwd) {
-    console.error("No such command or directory:", cwd);
-    return;
+if (!fs.existsSync("tsconfig.json")) {
+  var parent = __dirname.replace(/[\\\/][^\\\/]+[\\\/]?$/, "");
+  if (fs.existsSync(parent + "/tsconfig.json")) {
+    process.chdir(parent);
   }
-  require("typescript/lib/tsc");
-})();
-
-function popProcessArg(index, defaultValue) {
-  var arg = process.argv[index] || null;
-  if (arg != null) {
-    process.argv.splice(index, 1);
-  } else if (defaultValue != null) {
-    arg = defaultValue;
-  }
-  return arg;
 }
+require("typescript/lib/tsc");
