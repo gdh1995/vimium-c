047cc95d786a412df03572911b1fc80e2e46b391
gdh1995 <gdh1995@qq.com>
2019-11-21 00:20:07 +0800

    extend click: check VHints.hintKeystroke_ before reiniting it

--- content/extend_click.ts
+++ content/extend_click.ts
@@ -131,7 +131,7 @@ if (VDom && VimiumInjector === undefined) {
     }
     if (isFirstResolve & fromAttrs) {
       isFirstResolve ^= fromAttrs;
-      VHints.isActive_ && setTimeout(VHints.CheckLast_, 34);
+      VHints.isActive_ && !VHints.hintKeystroke_ && setTimeout(VHints.CheckLast_, 34);
     }
   }
   function resolve(isBox: BOOL, nodeIndexList: number[]): void {
@@ -640,7 +640,7 @@ _listen(kOnDomReady, doInit, !0);
       event && VDom && setTimeout(function (): void {
         if (!hasFindAll && VDom) {
           findAllOnClick(kContentCmd.AutoFindAllOnClick);
-          VHints.isActive_ && setTimeout(VHints.CheckLast_, 34);
+          VHints.isActive_ && !VHints.hintKeystroke_ && setTimeout(VHints.CheckLast_, 34);
         }
       }, GlobalConsts.ExtendClick_DelayToFindAll);
     }, 0, 1);
