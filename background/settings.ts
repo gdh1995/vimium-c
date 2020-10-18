c2724e5acf28cb02611a4054b31b8f46c564ee05
gdh1995 <gdh1995@qq.com>
2020-10-19 01:35:08 +0800

    add a default sed rule to convert URLs in m.jd

--- background/settings.ts
+++ background/settings.ts
@@ -308,7 +308,7 @@ var Settings_ = {
     __proto__: null as never,
     autoDarkMode: true,
     autoReduceMotion: false,
-    clipSub: "",
+    clipSub: "p@^https://item.m.jd.com/product/(\\d+).html\\b@https://item.jd.com/$1.html@",
     dialogMode: false,
     exclusionListenHash: true,
     exclusionOnlyFirstMatch: false,
