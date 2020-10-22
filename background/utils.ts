d28ba4ef9dc20428a4956b1d1c834f7c997f4b24
gdh1995 <gdh1995@qq.com>
2020-10-23 01:03:40 +0800

    math: support ＋−×÷, °, ', '' and "rad"

--- background/utils.ts
+++ background/utils.ts
@@ -385,13 +385,21 @@ var BgUtils_ = {
       ).then<Urls.MathEvalResult>(function (MathParser): Urls.MathEvalResult {
         BgUtils_.quotedStringRe_.test(path) && (path = path.slice(1, -1));
         path = path.replace(/\uff0c/g as RegExpG, " ");
-        path = path.replace(<RegExpG & RegExpSearchable<0>> /[\u2070-\u2079\xb2\xb3\xb9]+/g, (str): string => {
-          let out = ""
+        const re2 = /([\u2070-\u2079\xb2\xb3\xb9]+)|[\xb0\uff0b\u2212\xd7\xf7]|''?/g
+        path = path.replace(<RegExpG & RegExpSearchable<0>> /[\xb0']\s*\d+(\s*)(?=\)|$)/g, (str, g1): string => {
+          str = str.trim()
+          return str + (str[0] === "'"  ? "''" : "'") + g1
+        }).replace(<RegExpG & RegExpSearchable<1>> re2, (str, g1): string => {
+          let out = "", i: number
+          if (!g1) {
+            return str === "\xb0" ? "/180*PI+" : (i = "\uff0b\u2212\xd7\xf7".indexOf(str)) >= 0 ? "+-*/"[i]
+                : `/${str === "''" ? 3600 : 60}/180*PI+`
+          }
           for (const ch of str) {
             out += ch < "\xba" ? ch > "\xb3" ? 1 : ch < "\xb3" ? 2 : 3 : ch.charCodeAt(0) - 0x2070
           }
           return out && "**" + out
-        })
+        }).replace(<RegExpG>/\*PI\+(?!\s*\d)/g, "*PI").replace(<RegExpG> /([\d.])rad\b/g, "$1")
         let result = BgUtils_.tryEvalMath_(path, MathParser) || "";
         return [result, Urls.kEval.math, path];
       });
